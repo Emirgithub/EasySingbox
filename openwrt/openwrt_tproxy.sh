86f79c6b93834f4847c43b4301d6190ebafd7da7
Emir <61544744+Emirgithub@users.noreply.github.com>
2024-12-07 14:03:46 +0800

    Update openwrt_tproxy.sh

--- openwrt/openwrt_tproxy.sh
+++ openwrt/openwrt_tproxy.sh
@@ -10,7 +10,7 @@
 # 配置参数
 BACKEND_URL="http://88.88.88.188:5000/"  # 转换后端地址
 SUBSCRIPTION_URL="ss://YWVzLTI1Ni1jZmI6YW1hem9uc2tyMDU%3D@52.195.210.20:443#TW%2C"  # 订阅地址
-TEMPLATE_URL="https://raw.githubusercontent.com/Emirgithub/EasySingbox/refs/heads/master/openwrt/openwrt_tproxy.sh"  # 配置文件（规则模板)
+TEMPLATE_URL="https://raw.githubusercontent.com/Emirgithub/SingboxRule/refs/heads/master/config/singbox/config_tproxy.json"  # 配置文件（规则模板)
 TPROXY_PORT=7895  # sing-box tproxy 端口，和配置文件（规则模板）里的端口一致！
 PROXY_FWMARK=1
 PROXY_ROUTE_TABLE=100
